add489f19d1d0705870589866e6dd16d9a919361
zhoulnationalgrid <liming.zhou@nationalgrid.com>
2014-09-10 15:46:30 -0400

    Create metadata.rb

---
+++ mongodb-mysite/metadata.rb
@@ -0,0 +1,7 @@
+name        "mongodb-mysite"
+description 'Example cookbook for MongoDB Berkshelf based install'
+maintainer  "MyOrg"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
